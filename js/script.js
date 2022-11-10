6b283849f1e9e955d105b809013556664d5d6fcf
Alexandr Zhdanko <zhdko@yandex.ru>
2022-11-10 05:55:33 +0400

    fix: validator bugfix

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 let content = document.querySelector('.content');
-let editButton = content.querySelector('.button-icon__edit');
+let editButton = content.querySelector('.button-icon_action_edit');
 let popup = content.querySelector('.popup');
 let saveButton = popup.querySelector('.popup__submit-btn_place_edit-form');
-let closeButton = popup.querySelector('.button-icon__close')
+let closeButton = popup.querySelector('.button-icon_action_close')
 
 function openPopup() {
   popup.classList.toggle('popup_opened')
