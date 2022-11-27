196d24e7cf8ad369f837d2fcb2f6b56647678754
Alexandr Zhdanko <zhdko@yandex.ru>
2022-11-27 16:46:25 +0400

    refactor: transfer an array of cards to cards.js

---
+++ scripts/cards.js
@@ -0,0 +1,26 @@
+const galleryList = [
+  {
+    link: 'https://images.unsplash.com/photo-1538590888307-8417d434e15f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80',
+    title: 'Хребет Ацунта',
+  },
+  {
+    link: 'https://images.unsplash.com/photo-1629617708610-ea01331d53ac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80',
+    title: 'Троицкая церковь Гергети',
+  },
+  {
+    link: 'https://images.unsplash.com/photo-1543076499-a6133cb932fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80',
+    title: 'Мартвили',
+  },
+  {
+    link: 'https://images.unsplash.com/photo-1563284223-333497472e88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80',
+    title: 'Казбеги',
+  },
+  {
+    link: 'https://images.unsplash.com/photo-1621868811134-2548d9e7f147?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80',
+    title: 'Тбилиси',
+  },
+  {
+    link: 'https://images.unsplash.com/photo-1489362059928-00f6f10663bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1430&q=80',
+    title: 'Гудуари',
+  },
+];
