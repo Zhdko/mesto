857e0777b29342c5cd3277506ba7f997136f61be
Alexandr Zhdanko <zhdko@yandex.ru>
2023-01-18 19:08:54 +0400

    fix: изменение логики объявления и вызова слушателей

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -5,15 +5,15 @@ export class Popup {
 
   open() {
     this._popup.classList.add('popup_opened');
-    document.addEventListener('keydown', (evt) => this._handleEscClose(evt))
+    document.addEventListener('keydown', this._handleEscClose)
   }
 
   close() {
     this._popup.classList.remove('popup_opened');
-    document.removeEventListener('keydown', (evt) => this._handleEscClose(evt))
+    document.removeEventListener('keydown', this._handleEscClose)
   }
 
-  _handleEscClose(evt) {
+  _handleEscClose = (evt) => {
     if (evt.key === 'Escape') {
       this.close();
     }
